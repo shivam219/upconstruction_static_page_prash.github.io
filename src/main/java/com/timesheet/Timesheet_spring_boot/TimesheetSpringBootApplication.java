5ec0424643c02c2e23cd4d936acb5e4078f2d18d
shivam219 <61514793+shivam219@users.noreply.github.com>
2022-09-02 15:30:41 +0530

    main

---
+++ src/main/java/com/timesheet/Timesheet_spring_boot/TimesheetSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.timesheet.Timesheet_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timesheet/Timesheet_spring_boot/TimesheetSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timesheet.Timesheet_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
